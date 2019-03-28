e689ac2bae7278feeeb329b9b87c7c7e694c41a6
Tobias Emrich <tobiemrich@gmail.com>
2019-03-28 17:04:46 +0100

    Changed to slivers

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -23,57 +23,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.person),
-          color: Colors.white,
-          onPressed: _bloc.signIn,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.directions_run),
-            color: Colors.white,
-            onPressed: _bloc.signOut,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            centerTitle: false,
+            title: Text('Übersicht'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.person),
+                color: Colors.white,
+                onPressed: _bloc.signIn,
+              ),
+              IconButton(
+                icon: Icon(Icons.directions_run),
+                color: Colors.white,
+                onPressed: _bloc.signOut,
+              ),
+            ],
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [AppColors.PURPLE, AppColors.BLUE])),
+            ),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(50),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  child: Container(
+                    child: Text('HALLO'),
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+            snap: true,
+            floating: true,
           ),
-        ],
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [AppColors.PURPLE, AppColors.BLUE])),
-        ),
-        elevation: 0,
-      ),
-      body: Center(
-        child: StreamBuilder<List<DataEntry>>(
-          stream: _bloc.allEntries,
-          initialData: [],
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              print(snapshot.error);
-              return Text('Fehler!');
-            } else if (!snapshot.hasData) {
-              return Text('Keine Daten!');
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, i) {
-                  return Card(
-                    child: ListTile(
-                      title: Text(snapshot.data[i].title),
-                      subtitle: Text(snapshot.data[i].date.toIso8601String()),
-                      trailing: Text('${snapshot.data[i].value}'),
-                      leading: Text(snapshot.data[i].name.substring(0, 1)),
-                    ),
+          StreamBuilder<List<DataEntry>>(
+              stream: _bloc.allEntries,
+              initialData: [],
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                  return SliverToBoxAdapter(child: Text('Fehler!'));
+                } else if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                      child: Center(child: Text('Keine Daten!')));
+                } else {
+                  return SliverList(
+                    delegate: SliverChildBuilderDelegate((context, i) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(snapshot.data[i].title),
+                          subtitle:
+                              Text(snapshot.data[i].date.toIso8601String()),
+                          trailing: Text('${snapshot.data[i].value}'),
+                          leading: Text(snapshot.data[i].name.substring(0, 1)),
+                        ),
+                      );
+                    }, childCount: snapshot.data.length),
                   );
-                },
-              );
-            }
-          },
-        ),
+                }
+              }),
+        ],
       ),
     );
   }
