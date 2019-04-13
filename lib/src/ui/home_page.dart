423741f090ed8a87b14ef3f9a0bd22e7afb00326
Tobias Emrich <tobiemrich@gmail.com>
2019-04-13 12:02:57 +0200

    Changed slivers back to normal list

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -23,75 +23,69 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            centerTitle: false,
-            title: Text('Übersicht'),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.person),
-                color: Colors.white,
-                onPressed: _bloc.signIn,
-              ),
-              IconButton(
-                icon: Icon(Icons.directions_run),
-                color: Colors.white,
-                onPressed: _bloc.signOut,
-              ),
-            ],
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [AppColors.PURPLE, AppColors.BLUE])),
-            ),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(50),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 40,
-                  child: Container(
-                    child: Text('HALLO'),
-                    color: Colors.red,
-                  ),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Übersicht'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person),
+            color: Colors.white,
+            onPressed: _bloc.signIn,
+          ),
+          IconButton(
+            icon: Icon(Icons.directions_run),
+            color: Colors.white,
+            onPressed: _bloc.signOut,
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [AppColors.PURPLE, AppColors.BLUE])),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(40),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 40,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
             ),
-            snap: true,
-            floating: true,
           ),
-          StreamBuilder<List<DataEntry>>(
-              stream: _bloc.allEntries,
-              initialData: [],
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  print(snapshot.error);
-                  return SliverToBoxAdapter(child: Text('Fehler!'));
-                } else if (!snapshot.hasData) {
-                  return SliverToBoxAdapter(
-                      child: Center(child: Text('Keine Daten!')));
-                } else {
-                  return SliverList(
-                    delegate: SliverChildBuilderDelegate((context, i) {
-                      return Card(
-                        child: ListTile(
-                          title: Text(snapshot.data[i].title),
-                          subtitle:
-                              Text(snapshot.data[i].date.toIso8601String()),
-                          trailing: Text('${snapshot.data[i].value}'),
-                          leading: Text(snapshot.data[i].name.substring(0, 1)),
-                        ),
-                      );
-                    }, childCount: snapshot.data.length),
-                  );
-                }
-              }),
-        ],
+        ),
       ),
+      body: StreamBuilder<List<DataEntry>>(
+          stream: _bloc.allEntries,
+          initialData: [],
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return Text('Fehler!');
+            } else if (!snapshot.hasData) {
+              return Center(child: Text('Keine Daten!'));
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, i) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(snapshot.data[i].title),
+                        subtitle: Text(snapshot.data[i].date.toIso8601String()),
+                        trailing: Text('${snapshot.data[i].value}'),
+                        leading: Text(snapshot.data[i].name.substring(0, 1)),
+                      ),
+                    );
+                  });
+            }
+          }),
     );
   }
 }
